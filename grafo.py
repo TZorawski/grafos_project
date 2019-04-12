915d5a2cb1baaf4cb5f6f88afcc32a2f1ee1720d
Thais Zorawski <thaiszorawski@gmail.com>
2019-04-11 22:55:14 -0300

    Implementação de add_aresta, add_vertice, get_vertice, get ordem

--- grafo.py
+++ grafo.py
@@ -3,17 +3,38 @@ import sys
 from vertice import Vertice
 from aresta import Aresta
 
-class grafo ():
-    def __init__(self):
-        self.num_arestas = 0
+class Grafo ():
+    def __init__(self, is_digrafo):
         self.lista_vertices = []
+        self.lista_arestas = []
+        # implementar matriz de adjacencia
+        self.is_digrafo = is_digrafo
 
+    def add_vertice (self, vertice):
+        self.lista_vertices.append(vertice)
+    
+    def add_aresta (self, vertice1, vertice2, rotulo, valor):
+        a1 = Aresta(vertice1, vertice2, rotulo, valor)
+        self.lista_arestas.append(a1)
 
+    def get_vertice (self, rotulo):
+        for v in self.lista_vertices:
+            if(v.rotulo == rotulo):
+                return v
 
+    # def get_aresta (self, rotulo):
+    #     for v in self.lista_vertices:
+    #         if(v.rotulo == rotulo):
+    #             return v
+    
+    def get_ordem (self):
+        return len(self.lista_vertices)
 
+    #def get_grau (self, vertice):
+        # implementar matriz de adjacencia primeiro
 
 # def add_node(label):
-#     grafo = [[0]]
+# #     grafo = [[0]]
 #     grafo[0].append(label)
 #     n = [label]
 #     for i in range (len(grafo[0])-1):
