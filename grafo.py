8377480b5e75e4cdbbd37a0f0e550fb94c50287f
Thais Zorawski <thaiszorawski@gmail.com>
2019-04-11 22:16:41 -0300

    Passagem para OO

--- grafo.py
+++ grafo.py
@@ -1,21 +1,31 @@
 # -*- coding: utf-8 -*-
 import sys
-grafo = [[0]]
+from vertice import Vertice
+from aresta import Aresta
+
+class grafo ():
+    def __init__(self):
+        self.num_arestas = 0
+        self.lista_vertices = []
 
 
-def add_node(label):
-    grafo[0].append(label)
-    n = [label]
-    for i in range (len(grafo[0])-1):
-        n.append(0)
-    grafo.append(n)
 
-    for node in grafo[1::]:
-        node.append(0)
+
+
+# def add_node(label):
+#     grafo = [[0]]
+#     grafo[0].append(label)
+#     n = [label]
+#     for i in range (len(grafo[0])-1):
+#         n.append(0)
+#     grafo.append(n)
+
+#     for node in grafo[1::]:
+#         node.append(0)
     
-    print(grafo)
+#     print(grafo)
 
-add_node('a1')
-add_node('b1')
-add_node('c1')
-add_node('d1')
+# add_node('a1')
+# add_node('b1')
+# add_node('c1')
+# add_node('d1')
