a39891cf96d61c5f42803ea6dcbc6d17a1d6e9b2
Thais Zorawski <thaiszorawski@gmail.com>
2019-07-05 10:59:29 -0300

    Exemplo de requisições

--- aplicacao.py
+++ aplicacao.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import sys
-import grafo.vertice
+# import grafo.vertice
 import json
 import requests
 
@@ -9,8 +9,22 @@ class Aplicacao ():
         self.nome = ""
 
 def main ():
-  response = requests.get("http://jsonplaceholder.typicode.com/comments")
-  # response = requests.get("http://gateway.marvel.com/v1/public/comics")
+  # Exemplo REQUEST
+  # response = requests.get("http://jsonplaceholder.typicode.com/comments")
+
+  # REQUEST para github
+  # response = requests.get(
+  #    'https://api.github.com/search/repositories',
+  #    params=b'q=requests+language:python',
+  # )
+
+  # REQUEST para Marvel
+  parametros = {
+  "apikey": "2fd86138ee4056a8d6f596e7fda4d0e6",
+  "ts": "04/07/201922:39",
+  "hash": "5c6402e11e0fb933909e9057c87a0b94"
+  }
+  response = requests.get("https://gateway.marvel.com:443/v1/public/characters?", params=parametros)
   print(response.json())
 
 main()
