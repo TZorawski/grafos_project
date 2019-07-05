839ad93e3f3107f651630cd5c98d3d87df33ca31
Thais Zorawski <thaiszorawski@gmail.com>
2019-07-05 11:17:00 -0300

    Pegando dados dos personagens

--- aplicacao.py
+++ aplicacao.py
@@ -24,7 +24,19 @@ def main ():
   "ts": "04/07/201922:39",
   "hash": "5c6402e11e0fb933909e9057c87a0b94"
   }
-  response = requests.get("https://gateway.marvel.com:443/v1/public/characters?", params=parametros)
-  print(response.json())
+
+  # Pega todos os personagens
+  # response = requests.get("https://gateway.marvel.com:443/v1/public/characters?", params=parametros)
+  
+  # Pega o personagem 
+  response = requests.get("https://gateway.marvel.com:443/v1/public/characters/1017100?", params=parametros)
+
+  # print(response.json().data.results[0].description)
+  data = response.json()['data']
+  results = data['results'][0]
+  name = results['name']
+  description = results['description']
+  print(name)
+  print(description)
 
 main()
