eab5d8bbf58b7f471cadad6e537298a5107d735a
LuccaGit <lucasrocha.2017@alunos.utfpr.edu.br>
2019-07-10 11:41:29 -0300

    Grafo populado: nome, descrição, comics e stories

--- aplicacao.py
+++ aplicacao.py
@@ -5,62 +5,83 @@ import json
 import requests
 import networkx as nx
 
-class Aplicacao ():
-    def __init__(self):
-        self.nome = ""
+class Character():
+    def __init__(self, name, description, comics, stories):
+        self.__name = name
+        self.__description = description
+        self.__comics = comics
+        self.__stories = stories
+
+    def setName(self, name):
+        self.__name = name
+
+    def getName(self):
+        return self.__name
+
+    def setDescription(self, description):
+        self.__description = description
+
+    def getDescription(self):
+        return self.__description
+
+    def setComics(self, comics):
+        self.__comics = comics
+
+    def getComics(self):
+        return self.__comics
+
+    def setStories(self, stories):
+        self.__stories = stories
+
+    def getStories(self):
+        return self.__stories
 
 def main ():
-  # Exemplo REQUEST
-  # response = requests.get("http://jsonplaceholder.typicode.com/comments")
+    # Exemplo REQUEST
+    # response = requests.get("http://jsonplaceholder.typicode.com/comments")
 
-  # REQUEST para github
-  # response = requests.get(
-  #    'https://api.github.com/search/repositories',
-  #    params=b'q=requests+language:python',
-  # )
-  # print(response.json())
+    # REQUEST para github
+    # response = requests.get(
+    #    'https://api.github.com/search/repositories',
+    #    params=b'q=requests+language:python',
+    # )
+    # print(response.json())
 
-  # REQUEST para Marvel
-  # { "apikey": chave pública da api,
-  # "ts": timestamp,
-  # "hash": md5(timestamp+chave privada+chave publica) }
-  parametros = {
-  "apikey": "2fd86138ee4056a8d6f596e7fda4d0e6",
-  "ts": "04/07/201922:39",
-  "hash": "5c6402e11e0fb933909e9057c87a0b94"
-  }
+    # REQUEST para Marvel
+    # { "apikey": chave pública da api,
+    # "ts": timestamp,
+    # "hash": md5(timestamp+chave privada+chave publica) }
+    parametros = {
+    "apikey": "2fd86138ee4056a8d6f596e7fda4d0e6",
+    "ts": "04/07/201922:39",
+    "hash": "5c6402e11e0fb933909e9057c87a0b94"
+    }
 
-  # Pega todos os personagens
-  # Limite = 100 personagens
-  response = requests.get("https://gateway.marvel.com:443/v1/public/characters?limit=100", params=parametros)
-  # print(response.json())
+    # Pega todos os personagens -------------------------------------------------
+    # Limite = 100 personagens
+    response = requests.get("https://gateway.marvel.com:443/v1/public/characters?limit=10", params=parametros)
+    # print(response.json())
 
-  # Pega o personagem
-  # response = requests.get("https://gateway.marvel.com:443/v1/public/characters/1017100?", params=parametros)
-  #
-  # # print(response.json().data.results[0].description)
-  data = response.json()['data']
-  results = data['results']
-  for i in results:
-      print (i)
-      print("")
-#   name = results['name']
-#   print(results)
-  # description = results['description']
-  # print(name)
-  # print(description)
+    # Pega o personagem ---------------------------------------------------------
+    # response = requests.get("https://gateway.marvel.com:443/v1/public/characters/1017100?", params=parametros)
 
-#   G = nx.Graph() # adicionando um Grafo em networkX
+    # # print(response.json().data.results[0].description)
 
-# Adiciona os dados da API no Grafo -------------------------------------------------------------------------------------
-# PROBLEMA: não está aceitando o objeto completo, por enquanto está populando o grafo somente com o nome dos personagens.
-#   for v in data['results']:
-#     # print v['name']
-#     G.add_node(v['name']) # adicionando um Nó em networkX
+    data = response.json()['data']
+    # results = data['results']
 
-# # Printa os nós(nomes dos personagens) a partir do grafo
-#   for node in G:
-#     print node
+    G = nx.Graph() # adicionando um Grafo em networkX
+    obj = []
+    # Adiciona os dados da API no Grafo -------------------------------------------------------------------------------------
+    # PROBLEMA: não está aceitando o objeto completo, por enquanto está populando o grafo somente com o nome dos personagens.
+    for v in data['results']:
+        obj = Character(v['name'], v['description'], v['comics'], v['stories'])
+        G.add_node(obj) # adicionando um Nó em networkX
+
+    # Printa os nós(nomes dos personagens) a partir do grafo
+    for node in G:
+        print("----------------------- Comics ---------------------------")
+        print(node.getComics())
 
 main()
 
